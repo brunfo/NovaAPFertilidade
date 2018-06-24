ffdfe66188d5ce05432befc491567aa2a71e637c
brunfo <bruno.rego.1976@gmail.com>
2018-06-24 11:29:06 +0000

    melhorias na interface

--- src/pt/novaapfertilidade/gui/GUI.java
+++ src/pt/novaapfertilidade/gui/GUI.java
@@ -1,14 +1,26 @@
 package pt.novaapfertilidade.gui;
 
 import pt.novaapfertilidade.apf.APFertilidade;
+import pt.novaapfertilidade.model.Parceiro;
 
 public interface GUI {
-    //metodo para definições de sistema de utilização
+    /**
+     * Mmetodo para definições de sistema de utilização
+    */
     static GUI getInstance() {
         if (APFertilidade.getUserInterface().equals("consola"))
             return Consola.getInstance();
         //TODO futuras interfaces de utilização
         return null;
+    }
+    
+    /**
+     * Cria um novo parceiro, através de inputs do utilizador.
+     * Avalia quais as configurações de interação com utilizador.
+     */
+    static void criaParceiro(Parceiro novoParceiro){
+        if (getInstance() instanceof Consola)
+            ((Consola) getInstance()).criaParceiro(novoParceiro);
     }
 
     void iniciar();
